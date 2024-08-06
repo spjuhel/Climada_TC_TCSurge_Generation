24694ec368d5efe3611c87617978ae422b8a4622
spjuhel <pro@sjuhel.org>
2024-08-06 17:42:31 +0200

    fix new wildcards

--- workflow/scripts/create_basin_5_year_period.py
+++ workflow/scripts/create_basin_5_year_period.py
@@ -34,8 +34,7 @@ sys.excepthook = handle_exception
 
 logger.info(f"Getting TC tracks for genesis basin {snakemake.wildcards.basin} for {snakemake.wildcards.years}")
 
-year_start, year_end = snakemake.wildcards.years.split("_")
-tracks = TCTracks.from_ibtracs_netcdf(year_range=(int(year_start), int(year_end)), genesis_basin=snakemake.wildcards.basin)
+tracks = TCTracks.from_ibtracs_netcdf(year_range=(int(snakemake.wildcards.start), int(snakemake.wildcards.end)), genesis_basin=snakemake.wildcards.basin)
 
 logger.info(f"Loading global centroids from {snakemake.input.global_cent}")
 cent = Centroids.from_hdf5(snakemake.input.global_cent)
