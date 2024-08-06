520bfe3bfa4d5240c000f5faf155357ee5f55616
spjuhel <pro@sjuhel.org>
2024-08-06 18:28:44 +0200

    concat all basins

---
+++ workflow/scripts/concatenate_all_basins.py
@@ -0,0 +1,38 @@
+import sys
+import logging, traceback
+
+from climada.hazard import TropCyclone
+
+logging.basicConfig(
+    filename=snakemake.log[0],
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.error(
+        "".join(
+            [
+                "Uncaught exception: ",
+                *traceback.format_exception(exc_type, exc_value, exc_traceback),
+            ]
+        )
+    )
+
+
+
+# Install exception handler
+sys.excepthook = handle_exception
+
+logger.info(f"Regrouping all basins")
+tcs = TropCyclone.concat([TropCyclone.from_hdf5(tcfile) for tcfile in snakemake.input])
+logger.info(f"Writing to {snakemake.output}")
+tcs.write_hdf5(snakemake.output)
