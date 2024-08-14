e31d9388a7e6673538944b775494516caa9d0a41
spjuhel <pro@sjuhel.org>
2024-08-14 18:16:22 +0200

    new scaling process

--- workflow/scripts/generate_TCs_basin_year.py
+++ workflow/scripts/generate_TCs_basin_year.py
@@ -6,7 +6,6 @@ from pathlib import Path
 import numpy as np
 
 from climada.hazard import TCTracks, TropCyclone, Centroids
-from pathos.pools import ProcessPool as Pool
 
 logging.basicConfig(
     filename=snakemake.log[0],
@@ -44,7 +43,6 @@ if os.stat(snakemake.input[0]).st_size == 0:
     logger.info(f"File is empty, which probably means there is no track data for this basin-year. Ignoring")
     Path(snakemake.output[0]).touch()
 else:
-    pool = Pool(nodes=snakemake.threads)
     logger.info(f"Created multiprocess pool with {pool.ncpus} cpus.")
     tracks = TCTracks.from_hdf5(snakemake.input.tracks)
     logger.info(f"There are {len(tracks.data)} tracks.")
@@ -56,11 +54,16 @@ else:
     cent_tracks = cent.select(extent=tracks.get_extent(snakemake.params.buf))
 
     logger.info(f"Computing TC wind-fields")
-    tc = TropCyclone.from_tracks(tracks, centroids=cent_tracks, pool=pool, max_memory_gb=snakemake.params.max_memory_gb)
-    freq_corr = 1/snakemake.config["nsynth"]
-    tc.frequency = np.ones(tc.event_id.size)*freq_corr
-    pool.close()
-    pool.join()
+    tclist = []
+    for n in range(0, tracks.size, snakemake.params.batch_size):
+        tr = copy.deepcopy(tracks)
+        tr.data = tr.data[n:n+snakemake.params.batch_size]
+        tr.equal_timestep(0.5)
+        tc = TropCyclone.from_tracks(tr, centroids=cent_tracks, max_memory_gb=snakemake.params.max_memory_gb)
+        freq_corr = 1/snakemake.config["nsynth"]
+        tc.frequency = np.ones(tc.event_id.size)*freq_corr
+        tclist.append(tc)
 
+    tc = TropCyclone.concat(tclist)
     logger.info(f"Writing to {snakemake.output[0]}")
     tc.write_hdf5(snakemake.output[0])
