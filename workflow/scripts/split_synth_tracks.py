ba6ddada9baf8896e48d2e9a83fe82e8403de83a
spjuhel <pro@sjuhel.org>
2024-08-20 10:46:25 +0200

    split track script

---
+++ workflow/scripts/split_synth_tracks.py
@@ -0,0 +1,62 @@
+import sys
+import logging, traceback
+import os
+import copy
+
+from pathlib import Path
+
+from climada.hazard import TCTracks
+
+logging.basicConfig(
+    filename=snakemake.log[0],
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.error(
+        "".join(
+            [
+                "Uncaught exception: ",
+                *traceback.format_exception(exc_type, exc_value, exc_traceback),
+            ]
+        )
+    )
+
+
+# Install exception handler
+sys.excepthook = handle_exception
+logger.info(f"Loading TC tracks from {snakemake.input.tracks}")
+
+outdir = Path(snakemake.output[0])
+max_tracks = snakemake.params.max_tracks
+
+if os.stat(snakemake.input[0]).st_size == 0:
+    logger.info(
+        f"File is empty, which probably means there is no track data for this basin-year. Ignoring."
+    )
+    Path(snakemake.output[0]).touch()
+else:
+    tracks = TCTracks.from_hdf5(snakemake.input.tracks)
+    logger.info(f"There are {len(tracks.data)} tracks.")
+    split = 1
+    for n in range(0, tracks.size, max_tracks):
+        logger.info(f"Splitting {n}:{n+max_tracks} tracks")
+        tr = copy.deepcopy(tracks)
+        tr.data = tr.data[n : n + max_tracks]
+        filename = (
+            outdir
+            / f"TCs_{snakemake.wildcards.basin}_{snakemake.wildcards.year}_split_{split}.hdf5"
+        )
+        logger.info(f"Writing to {filename}")
+        tr.write_hdf5(filename)
+        split += 1
