db19cd321257d94ab0a4596947c322d4d5fb9de6
spjuhel <pro@sjuhel.org>
2024-08-23 08:36:01 +0200

    slightly too far

--- workflow/scripts/generate_TCs_surges_basin_year.py
+++ workflow/scripts/generate_TCs_surges_basin_year.py
@@ -36,13 +36,11 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 
 basin = snakemake.wildcards.basin
 year = snakemake.wildcards.year
-tropcyc = snakemake.input.tropcyc[0]
+tropcyc = snakemake.input.tropcyc
 slr = snakemake.input.slr[0]
 slr_year = snakemake.wildcards.slr_year
-dem_topo_path = snakemake.input.dem[0]
+dem_topo_path = snakemake.input.dem
 
-print(slr)
-print(type(slr))
 # Install exception handler
 sys.excepthook = handle_exception
 logger.info(f"Computing TC Surge events for genesis basin {basin} for {year}")
