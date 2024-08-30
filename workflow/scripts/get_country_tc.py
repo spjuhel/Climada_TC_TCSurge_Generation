a1a94b80c8160a986d348985fde0ae975a8e26fa
spjuhel <pro@sjuhel.org>
2024-08-30 10:43:47 +0200

    split TCs by country

--- workflow/scripts/get_country_tc.py
+++ workflow/scripts/get_country_tc.py
@@ -2,6 +2,7 @@ import sys
 import logging, traceback
 
 import numpy as np
+import country_converter as coco
 
 from climada.hazard import Hazard
 
@@ -36,10 +37,13 @@ sys.excepthook = handle_exception
 
 climate_scenario = snakemake.wildcards.climate_scenario
 global_haz = snakemake.input.global_haz
+cc = coco.CountryConverter()
+countries = snakemake.params.countries
 
+countries_iso = cc.convert(countries, to="ISOnumeric")
 logger.info(f"Splitting TC events in countries for {climate_scenario} from {global_haz}")
 haz = Hazard.from_hdf5(global_haz)
-for cnt_id in np.unique(haz.centroids.region_id):
+for country, cnt_id in zip(countries_iso, countries):
     haz_cnt = haz.select(reg_id=cnt_id)
-    filename = f'tropcyc/tropcyc_{str(cnt_id)}_{climate_scenario}.hdf5'
+    filename = f'tropcyc/{climate_scenario}/tropcyc_{country}_{climate_scenario}.hdf5'
     haz_cnt.write_hdf5(filename)
