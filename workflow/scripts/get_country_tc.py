94ee7f2f8f938eb974926c52565bd6825229007f
Samuel Juhel <10011382+spjuhel@users.noreply.github.com>
2024-10-02 16:48:45 +0200

    Accept "all" for countries parameter

--- workflow/scripts/get_country_tc.py
+++ workflow/scripts/get_country_tc.py
@@ -43,12 +43,18 @@ start_period = snakemake.params.start_period
 end_period = snakemake.params.end_period
 countries = snakemake.params.countries
 
-cc = coco.CountryConverter()
-countries_iso_num = cc.convert(countries, to="ISOnumeric")
+if countries != "all":
+    cc = coco.CountryConverter()
+    countries_iso_num = cc.convert(countries, to="ISOnumeric")
 
 logger.info(f"Splitting TC events in countries for {climate_scenario} from {global_haz}")
 haz = TropCyclone.from_hdf5(global_haz)
 haz.centroids.set_region_id()
+if countries == "all":
+    countries_tmp = cc.convert(haz.centroids.gdf["region_id"].unique(), to="ISO3", src="ISOnumeric")
+    countries = [c for c in countries_tmp if c != "not found"]
+    countries_iso_num = cc.convert(countries, to="ISOnumeric")
+    
 for cnt_id, country in zip(countries_iso_num, countries):
     haz_cnt = haz.select(reg_id=cnt_id)
     filename = f'tropcyc/{climate_scenario}/by_country/tropcyc_{tc_res_str}arcsec_{nsynth}synth_{country}_{start_period}_to_{end_period}_{climate_scenario}.hdf5'
